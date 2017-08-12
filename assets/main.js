3523382c239b73f1febbdd7d7c4481293b796f20
Jonny Hork <JonnyHork@gmail.com>
2017-08-12 12:53:25 -0600

    Brush color and styling complete

--- assets/main.js
+++ assets/main.js
@@ -1,42 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   console.log('main.js is linked');
 
   // THIS CREATES THE CANVAS PIXELS
-for (let i = 0; i < 1953; i++){
-  let canvas = document.getElementById('canvas')
-  let pixel = document.createElement('div')
+  for (let i = 0; i < 1953; i++) {
+    const canvas = document.querySelector('.canvas')
+    let pixel = document.createElement('div')
     pixel.className = 'pixel'
     canvas.appendChild(pixel)
-}
+  }
 
-// COLOR PICKER EVENT HANDLE
-let currentColor
-// let brushColor = document.getElementsById('brushColor')
+  // COLOR PICKER EVENT HANDLE
+  let currentColor
+  let brushColor = document.querySelector('.current-color')
 
-const colorPalette = document.getElementById('color-select')
+  const colorSelect = document.querySelector('.color-select')
 
-colorPalette.addEventListener('click', function(){
-    console.log(this);
-  const selectedColor = event.target.getAttribute('id')
-  currentColor = selectedColor
+  colorSelect.addEventListener('click', function() {
+    // console.log(this);
+    const selectedColor = event.target.getAttribute('id')
+    currentColor = selectedColor
+    brushColor.setAttribute('id', currentColor)
 
-    console.log('The event.target color id:', selectedColor);
-    console.log('The currentColor is: ', currentColor);
+    // console.log('The event.target color id:', selectedColor);
+    // console.log('The currentColor is: ', currentColor);
+  })
+
+
+  // CANVAS EVENT HANDLE
+  const canvas = document.querySelector('.canvas')
+
+  canvas.addEventListener('click', function() {
+
+    let pixel = event.target
+    pixel.setAttribute('id', currentColor)
+  })
+
+
+
+
+
 })
 
-
-// CANVAS EVENT HANDLE
-const canvas = document.getElementById('canvas')
-
-canvas.addEventListener('click', function(){
-
-  let pixel = event.target
-  pixel.setAttribute('id', currentColor)
-})
-
-
-
-
-
-})
+// brushColor.setAttribute('id', currentColor)
+// brushColor.style.cssText()
